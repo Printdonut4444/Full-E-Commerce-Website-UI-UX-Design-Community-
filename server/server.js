87d86a32819b453b9f659f770fb48e55ad42c5f3
Chxnyut <chanyut@avision.co.th>
2026-03-11 16:02:33 +0700

    A: Add Server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const port = 5433;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        message: "kuy"
+    })
+});
+
+app.listen(5433, () => {
+    console.log(`Listen to port ${port}`);
+});
