c6bd8b40ced3be103eaaec186bca917d014c52be
Printdonut4444 <phuricha2944@gmail.com>
2026-03-12 13:39:23 +0700

    Donut

--- E_Commerce_Scipt/HomePage.js
+++ E_Commerce_Scipt/HomePage.js
@@ -27,3 +27,42 @@ window.onload = function () {
         });
     };
 }
+
+
+const profileBtn = document.getElementById('profileBtn');
+const dropdownMenu = document.getElementById('dropdownMenu');
+
+profileBtn.addEventListener('click', function(event) {
+  // สลับการเปิด/ปิดเมนู
+  dropdownMenu.classList.toggle('show');
+  
+  // สลับการค้างสีแดงที่ปุ่ม (Toggle คลาส active)
+  profileBtn.classList.toggle('active');
+  
+  event.stopPropagation();
+});
+
+// เมื่อคลิกที่อื่นนอกจากเมนู ให้ปิดเมนูและเอาสีแดงออก
+document.addEventListener('click', function(event) {
+  if (!profileBtn.contains(event.target) && !dropdownMenu.contains(event.target)) {
+    dropdownMenu.classList.remove('show');
+    profileBtn.classList.remove('active'); // เอาสีแดงออกเมื่อเมนูปิด
+  }
+});
+
+
+function onClickContact() {
+    window.location.href = "../Contact/Contact.html";
+}
+function onClickAbout() {
+    window.location.href = "../About/About.html";
+}
+function onClickSignUp() {
+    window.location.href = "../Sign-Up/Sign-Up.html";
+}
+function onClickAccount() {
+    window.location.href = "../Account/Account.html";
+}
+
+
+
