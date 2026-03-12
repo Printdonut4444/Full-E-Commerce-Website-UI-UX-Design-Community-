77b89d71b7adf301fc9fab2501e044f5c1bd361a
Chxnyut <chanyut@avision.co.th>
2026-03-12 13:40:21 +0700

    A: Update ProductDetailPage

--- E_Commerce_Scipt/ProductDetailsPage.js
+++ E_Commerce_Scipt/ProductDetailsPage.js
@@ -69,26 +69,26 @@ window.onload = () => {
     totalReviewsNode.innerText = `(${reviewCount} Reviews)`;
     totalReviewsNode.style.fontSize = "14px";
     totalReviewsNode.style.marginLeft = "10px";
-    totalReviewsNode.style.color = "#7D8184"
+    totalReviewsNode.style.color = "#7D8184";
 
     const spanLine = document.createElement("span");
     spanLine.innerText = "|";
     spanLine.style.marginLeft = "10px";
-    spanLine.style.color = "#7D8184"
+    spanLine.style.color = "#7D8184";
     totalReviewsNode.appendChild(spanLine);
 
     const stockStatus = "in Stock";
     const stockNode = document.getElementById("stock");
     stockNode.innerText = stockStatus;
-    stockNode.style.color = stockStatus === 'in Stock' ? "#00FF66" : "red"
-    stockNode.style.marginLeft = "10px"
-    stockNode.style.fontSize = "14px"
+    stockNode.style.color = stockStatus === 'in Stock' ? "#00FF66" : "red";
+    stockNode.style.marginLeft = "10px";
+    stockNode.style.fontSize = "14px";
 
     const price = 192;
     const priceNode = document.getElementById("price");
     priceNode.innerText = formatPriceToLocalFormat("EN", price);
 
-    const productDescription = "PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive."
+    const productDescription = "PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive.";
     const productDescNode = document.getElementById("product-desc");
     productDescNode.innerText = productDescription;
     productDescNode.style.fontSize = "14px";
@@ -103,13 +103,13 @@ window.onload = () => {
         childElement.style.height = "20px";
         childElement.style.width = "20px";
         childElement.style.borderRadius = "50%";
-        childElement.style.backgroundColor = item.color
+        childElement.style.backgroundColor = item.color;
         productColorNode.appendChild(childElement);
     });
 
     const productsSize = ["XS", "S", "M", "L", "XL"];
     const productsSizeNode = document.getElementById("product-size");
-    productsSize.forEach((item, index)=> {
+    productsSize.forEach((item, index) => {
         const childElelment = document.createElement("div");
         childElelment.innerText = item;
         childElelment.style.display = "flex";
@@ -117,7 +117,7 @@ window.onload = () => {
         childElelment.style.alignItems = "center";
         childElelment.style.fontSize = "14px";
         childElelment.style.padding = "4px";
-        childElelment.style.width = "32px"; 
+        childElelment.style.width = "32px";
         childElelment.style.height = "32px";
         childElelment.style.border = "1px solid black";
         childElelment.style.borderRadius = "6px";
@@ -130,20 +130,20 @@ window.onload = () => {
                 el.style.backgroundColor = "white";
                 el.style.color = "black";
                 el.style.borderColor = "black";
-            })
+            });
 
             childElelment.style.backgroundColor = "#DB4444";
             childElelment.style.color = "#FFFFFF";
             childElelment.style.borderColor = "#DB4444";
         });
-        productsSizeNode.appendChild(childElelment);    
+        productsSizeNode.appendChild(childElelment);
     });
 
     const productRelatedItem = [
-        {picture: "../Asset/img/ProductDetails/g92-2-500x500 1.png", name: "HAVIT HV-G92 Gamepad", originalPrice: 160, discountedRate: 0.40, discountedPrice: 120, rating: 5, totalReview: 88},
-        {picture: "../Asset/img/ProductDetails/Frame 612.png", name: "AK-900 Wired Keyboard", originalPrice: 1160, discountedRate: 0.35, discountedPrice: 960, rating: 4, totalReview: 75},
-        {picture: "../Asset/img/ProductDetails/Frame 613.png", name: "IPS LCD Gaming Monitor", originalPrice: 400, discountedRate: 0.30, discountedPrice: 370, rating: 5, totalReview: 99},
-        {picture: "../Asset/img/ProductDetails/Frame 610.png", name: "RGB liquid CPU Cooler", originalPrice: 170, discountedRate: 0.09, discountedPrice: 160, rating: 4, totalReview: 65},
+        { picture: "../Asset/img/ProductDetails/g92-2-500x500 1.png", name: "HAVIT HV-G92 Gamepad", originalPrice: 160, discountedRate: 0.40, discountedPrice: 120, rating: 5, totalReview: 88 },
+        { picture: "../Asset/img/ProductDetails/Frame 612.png", name: "AK-900 Wired Keyboard", originalPrice: 1160, discountedRate: 0.35, discountedPrice: 960, rating: 4, totalReview: 75 },
+        { picture: "../Asset/img/ProductDetails/Frame 613.png", name: "IPS LCD Gaming Monitor", originalPrice: 400, discountedRate: 0.30, discountedPrice: 370, rating: 5, totalReview: 99 },
+        { picture: "../Asset/img/ProductDetails/Frame 610.png", name: "RGB liquid CPU Cooler", originalPrice: 170, discountedRate: 0.09, discountedPrice: 160, rating: 4, totalReview: 65 },
     ];
 
     const relatedItemNode = document.getElementById("related-item");
@@ -191,25 +191,138 @@ window.onload = () => {
         likeButton.style.height = "34px";
         likeButton.style.borderRadius = "50%";
         likeButton.style.position = "absolute";
-        likeButton.style.right = "10px"; 
-        likeButton.style.top = "10px";   
+        likeButton.style.right = "10px";
+        likeButton.style.top = "10px";
+        likeButton.style.backgroundColor = "white";
+        likeButton.style.display = "flex";
+        likeButton.style.justifyContent = "center";
+        likeButton.style.alignItems = "center";
+        likeButton.style.cursor = "pointer";
+
+        const hartIcon = document.createElement("img");
+        hartIcon.src = "../Asset/img/ProductDetails/hart.png";
+        hartIcon.alt = "hart";
+        likeButton.appendChild(hartIcon);
+        child1.appendChild(likeButton);
+
+        const viewDetailButton = document.createElement("div");
+        viewDetailButton.style.width = "34px";
+        viewDetailButton.style.height = "34px";
+        viewDetailButton.style.borderRadius = "50%";
+        viewDetailButton.style.position = "absolute";
+        viewDetailButton.style.right = "10px";
+        viewDetailButton.style.top = "54px";
+        viewDetailButton.style.backgroundColor = "white";
+        viewDetailButton.style.display = "flex";
+        viewDetailButton.style.justifyContent = "center";
+        viewDetailButton.style.alignItems = "center";
+        viewDetailButton.style.cursor = "pointer";
+
+        const eyesIcon = document.createElement("img");
+        eyesIcon.src = "../Asset/img/ProductDetails/Group.png";
+        eyesIcon.alt = "eyes";
+        viewDetailButton.appendChild(eyesIcon);
+        child1.appendChild(viewDetailButton);
 
         rootChild.appendChild(child1);
-
 
         const child2 = document.createElement("div");
         child2.style.display = "flex";
         child2.style.flexDirection = "column";
         child2.style.justifyContent = "end";
         child2.style.alignItems = "start";
-        child2.style.backgroundColor = "orange";
+        child2.style.backgroundColor = "white";
         child2.style.height = "100px";
         child2.style.width = "100%";
+        child2.style.gap = "5px";
+
+        const nameNode = document.createElement("div");
+        const priceNode = document.createElement("div");
+        const ratingNode = document.createElement("div");
+
+        nameNode.style.fontSize = "16px";
+        nameNode.style.fontWeight = "bold";
+        nameNode.innerText = item.name;
+
+        priceNode.style.display = "flex";
+        priceNode.style.justifyContent = "start";
+        priceNode.style.alignItems = "center";
+        priceNode.style.gap = "10px";
+
+        const discountedPriceNode = document.createElement("span");
+        const originPriceNode = document.createElement("span");
+
+        discountedPriceNode.style.color = "#DB4444";
+        discountedPriceNode.innerText = formatPriceToLocalFormat("EN", item.discountedPrice);
+        discountedPriceNode.style.fontSize = "16px";
+        discountedPriceNode.style.fontWeight = "bold";
+
+        originPriceNode.style.color = "#7D8184";
+        originPriceNode.innerText = formatPriceToLocalFormat("EN", item.originalPrice);
+        originPriceNode.style.fontSize = "16px";
+        originPriceNode.style.fontWeight = "bold";
+        originPriceNode.style.textDecoration = "line-through";
+
+        priceNode.appendChild(discountedPriceNode);
+        priceNode.appendChild(originPriceNode);
+
+        ratingNode.style.display = "flex";
+        ratingNode.style.justifyContent = "start";
+        ratingNode.style.alignItems = "center";
+        ratingNode.style.gap = "10px";
+
+        const starRootNode = document.createElement("div");
+        starRootNode.style.display = "flex";
+        starRootNode.style.justifyContent = "start";
+        starRootNode.style.alignItems = "center";
+        const star = [];
+        const starOn = `
+                    <svg id="${item.name}" width="15px" height="30px" viewBox="0 0 130 130" version="1.1"
+                        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                        xml:space="preserve" xmlns:serif="http://www.serif.com/"
+                        style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+                        <g transform="matrix(1,0,0,1,-634.728,-382.568)">
+                            <path id="path-star1"
+                                d="M702.68,382.568L718.721,431.938L770.632,431.938L728.635,462.45L744.677,511.82L702.68,481.308L660.683,511.82L676.724,462.45L634.728,431.938L686.639,431.938L702.68,382.568Z"
+                                style="fill:#FFAD33;" />
+                        </g>
+                    </svg>`
+        const starOff = `
+                    <svg id="${item.name}" width="15px" height="30px" viewBox="0 0 130 130" version="1.1"
+                        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                        xml:space="preserve" xmlns:serif="http://www.serif.com/"
+                        style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+                        <g transform="matrix(1,0,0,1,-634.728,-382.568)">
+                            <path id="path-star5"
+                                d="M702.68,382.568L718.721,431.938L770.632,431.938L728.635,462.45L744.677,511.82L702.68,481.308L660.683,511.82L676.724,462.45L634.728,431.938L686.639,431.938L702.68,382.568Z"
+                                style="fill:rgb(197, 196, 191);" />
+                        </g>
+                    </svg>`
+        for (let i = 0; i < item.rating; i++) {
+            star.push(starOn);
+        }
+ 
+        for (let i = 0; i < (5 - item.rating); i++) {
+            star.push(starOff);
+        }
+
+        const starJoin = star.join("");
+        starRootNode.innerHTML = starJoin;
+
+        ratingNode.appendChild(starRootNode);
+        const reviewsNode = document.createElement("div");
+        reviewsNode.style.color = "#7D8184";
+        reviewsNode.innerText = `(${item.totalReview})`;
+        ratingNode.appendChild(reviewsNode);
+
+        child2.appendChild(nameNode);
+        child2.appendChild(priceNode);
+        child2.appendChild(ratingNode);
         rootChild.appendChild(child2);
 
 
         relatedItemNode.appendChild(rootChild);
-    })
+    });
 };
 
 const start1 = document.getElementById("star1");
@@ -261,7 +374,7 @@ start2.addEventListener("click", () => {
             path5.style.fill = "rgb(197, 196, 191)";
             window.star5 = false;
         }
-    }   
+    }
 });
 start3.addEventListener("click", () => {
     if (window.star3 === false) {
