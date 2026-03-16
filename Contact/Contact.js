cc35266a243ba7ec3ff7c466ba235691de4a231f
Chxnyut <chanyut@avision.co.th>
2026-03-16 14:56:39 +0700

    A: Fix layout to menu

---
+++ Contact/Contact.js
@@ -0,0 +1,17 @@
+function link(link) {
+  switch (link) {
+    case "Home":
+      window.location.href = "../E_Commerce_Page/HomePage.html";
+      break;
+    case "SignUp":
+      window.location.href = "../Sign-Up/Sign-Up.html";
+      break;
+    case "About":
+      window.location.href = "../About/About.html";
+      break;
+    case "Contact":
+      window.location.href = "../Contact/Contact.html";
+      break;
+
+  }
+}
--- E_Commerce_Scipt/WishlistPage.js
+++ E_Commerce_Scipt/WishlistPage.js
@@ -68,5 +68,22 @@ function onClickCart() {
   window.location.href = "../Cart/Cart.html";
 }
 
-
-
+function link(link) {
+  switch (link) {
+    case "Home":
+      window.location.href = "../E_Commerce_Page/HomePage.html";
+      break;
+    case "SignUp":
+      window.location.href = "../Sign-Up/Sign-Up.html";
+      break;
+    case "About":
+      window.location.href = "../About/About.html";
+      break;
+    case "Contact":
+      window.location.href = "../Contact/Contact.html";
+      break;
+    case "LogIn":
+      window.location.href = "../E_Commerce_Page/LoginPage.html";
+      break;
+  }
+}
