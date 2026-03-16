d29ef71218116a0b7e7c3c3f2b546dfb24d2c09d
Chxnyut <chanyut@avision.co.th>
2026-03-16 10:52:21 +0700

    A: Update ProductDetailsPage Complete.

--- E_Commerce_Scipt/ProductDetailsPage.js
+++ E_Commerce_Scipt/ProductDetailsPage.js
@@ -461,4 +461,21 @@ start5.addEventListener("click", () => {
     }
 });
 
+function link(link) {
+    switch(link) {
+        case "Home":
+            window.location.href = "../E_Commerce_Page/HomePage.html";
+            break;
+        case "SignUp":
+            window.location.href = "../Sign-Up/Sign-Up.html";
+            break;
+        case "About":
+            window.location.href = "../About/About.html";
+            break;
+        case "Contact":
+            window.location.href = "../Contact/Contact.html";
+            break;
+
+    }
+}
 
